aab67e8cf19b07e4c7c00d4842118c57f787aa90
emekach <chukwuemeka.chukwuma11@gmail.com>
2025-01-28 22:08:20 +0000

    chore: update dependecies

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 const { getData } = require('./controllers/getDataController');
 
 const app = express();
 
 app.use(cors());
-
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
